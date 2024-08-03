481445f5426aeeeb5fff65a9c9df2ecf376c6e77
Bascum Macik <bascummacik@gmail.com>
2024-08-03 12:24:29 -0500

    added react app to working atthis time app

---
+++ myapp/src/App.js
@@ -0,0 +1,26 @@
+
+import {useState} from "react";
+import axios from 'axios';
+
+
+
+function App() {
+
+  const [data, setData] = useState("");
+
+  const getData = async () => {
+    let newData = await axios.get("http://localhost:3000/up");
+    console.log(newData.data);
+    setData(newData.data);
+  }
+
+  getData();
+
+  return (
+    <>
+      <h2>{data}</h2>
+    </>
+  );
+}
+
+export default App;
