25dc4ab1d8313095fd0b259c965537268ad58bf0
Bascum Macik <bascummacik@gmail.com>
2024-08-17 13:26:17 -0500

    changed timeout

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const sql = require("mssql");
 require("dotenv").config();
 
-config = `Server=tcp:testappdatabase.database.windows.net,1433;Database=testappdb;Uid=testappAdmin;Pwd=${process.env.AZURE_SQL_PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;`
+config = `Server=tcp:testappdatabase.database.windows.net,1433;Database=testappdb;Uid=testappAdmin;Pwd=${process.env.AZURE_SQL_PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=120;`
 
 module.exports = {
     connect: () => sql.connect(config),
