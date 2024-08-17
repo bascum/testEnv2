0ce76e020279ab61519a2c16387cea9730961aa9
Bascum Macik <bascummacik@gmail.com>
2024-08-17 14:02:43 -0500

    added a console.log

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const sql = require("mssql");
 require("dotenv").config();
 
-config = `Server=tcp:testappdatabase.database.windows.net,1433;Database=testappdb;Uid=testappAdmin;Pwd=${process.env.AZURE_SQL_PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=120;`
-
+config = `Server=tcp:testappdatabase.database.windows.net,1433;Database=testappdb;Uid=testappAdmin;Pwd=${process.env.AZURE_SQL_PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=120;`;
 module.exports = {
-    connect: () => sql.connect(config),
+    connect: () => {
+        sql.connect(config)
+        console.log(config);
+    },
     sql,
 };
