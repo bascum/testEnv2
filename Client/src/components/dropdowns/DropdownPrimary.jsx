c89e6ee05d378fbfedca026e9570ef8549b13eae
Brandon <bcav47@gmail.com>
2024-09-30 18:57:52 -0700

    newticket page

--- Client/src/components/dropdowns/DropdownPrimary.jsx
+++ Client/src/components/dropdowns/DropdownPrimary.jsx
@@ -1,14 +1,19 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import PropTypes from 'prop-types';
 
-function DropdownPrimary() {
+function DropdownPrimary({ title }) {
   return (
-    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+    <DropdownButton id="dropdown-basic-button" title={title}>
       <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
       <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
       <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
     </DropdownButton>
   );
 }
+
+DropdownPrimary.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default DropdownPrimary;
--- Client/src/pages/NewTicket.jsx
+++ Client/src/pages/NewTicket.jsx
@@ -4,11 +4,29 @@ import { Header } from "../components/header/Header";
 export const NewTicket = () => {
   return (
     <>
-    <Header/>
-    <DropdownPrimary/>
-    <input className="form-control" type="text" placeholder="Default input" />
-
+      <Header />
+      <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-sm">
+              <DropdownPrimary title="Printer Number" />
+            </div>
+            <div className="col-sm">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Callback Number"
+              />
+            </div>
+            <DropdownPrimary title="Priority" />
+          </div>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Description"
+          />
+        </form>
+      </div>
     </>
   );
 };
-
