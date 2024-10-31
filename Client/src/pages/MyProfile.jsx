aebc1c72d8083f38615c231f2c892f40ac526124
Brandon <bcav47@gmail.com>
2024-10-30 21:49:09 -0700

    updated??

--- Client/src/pages/MyProfile.jsx
+++ Client/src/pages/MyProfile.jsx
@@ -1,12 +1,77 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const MyProfile = () => {
+  const [name, setName] = useState("");
+  const [employeeID, setEmployeeID] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [username, setUsername] = useState("");
+  const [departmentNumber, setDepartmentNumber] = useState("");
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  const getProfile = async () => {
+    let result = (await axios.get("/users/get_profile")).data;
+    console.log(result);
+    if (result.success == "yes") {
+      setName(result.name);
+      setEmployeeID(result.employeeID);
+      setPhoneNumber(result.phoneNumber);
+      setUsername(result.username);
+      setDepartmentNumber(result.departmentNumber);
+    }
+  };
+
   return (
     <div className="containter m-4">
       <div className="card shadow">
         <div className="card-body">
+          <form>
             <h5 className="card-title mb-4">My Profile</h5>
-            <p className="card-text">Name: John Doe</p>
+            <input
+              className="form-control"
+              type="text"
+              value={name}
+              aria-label="Disabled input example"
+              disabled
+              readOnly
+            ></input>
+            <input
+              className="form-control"
+              type="text"
+              value={employeeID}
+              aria-label="Disabled input example"
+              disabled
+              readOnly
+            ></input>
+            <input
+              className="form-control"
+              type="text"
+              value={phoneNumber}
+              aria-label="Disabled input example"
+              disabled
+              readOnly
+            ></input>
+            <input
+              className="form-control"
+              type="text"
+              value={username}
+              aria-label="Disabled input example"
+              disabled
+              readOnly
+            ></input>
+            <input
+              className="form-control"
+              type="text"
+              value={departmentNumber}
+              aria-label="Disabled input example"
+              disabled
+              readOnly
+            ></input>
+          </form>
         </div>
       </div>
     </div>
